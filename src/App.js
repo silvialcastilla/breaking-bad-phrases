65c55ba5e4d9c396682b3c4b6a708786a3c6f51a
Tropical23 <silope0302@gmail.com>
2020-04-03 13:29:58 +0200

    proyecto maquetado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, {useState, useEffect} from 'react';
-import styled from '@emotion/styled';
-import Frase from './components/Frase'
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import Frase from "./components/Frase";
 
 const Contenedor = styled.div`
-display: flex;
-align-items: center;
-padding-top: 5rem;
-flex-direction: column;
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
 `;
 
 const Boton = styled.button`
-  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
   background-size: 300px;
-  font-family:  Arial, Helvetica, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   color: #fff;
   margin-top: 3rem;
   padding: 1rem 3rem;
@@ -21,36 +26,33 @@ const Boton = styled.button`
   transition: background-size 0.8s ease;
 
   :hover {
-    cursor:pointer;
+    cursor: pointer;
     background-size: 400px;
   }
 `;
 
 function App() {
   //state de frases
-  const [frase,guardarFrase] = useState({})
+  const [frase, guardarFrase] = useState({});
 
   const consultarAPI = async () => {
-    const api = await fetch ('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
-    const frase = await api.json() 
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
     guardarFrase(frase[0]);
-  }
+  };
 
   //cargar una frase
 
-  useEffect(()=>{
+  useEffect(() => {
     consultarAPI();
   }, []);
 
   return (
     <Contenedor>
-      <Frase
-      frase={frase}
-      />
-      <Boton
-        onClick={consultarAPI}
-      >Obtener frase
-      </Boton>
+      <Frase frase={frase} />
+      <Boton onClick={consultarAPI}>Obtener frase</Boton>
     </Contenedor>
   );
 }
